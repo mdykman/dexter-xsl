ff1e7b41086c65b9bbffb1b04a13818a1d01bf7f
mdykman <mdykman@8eee1ad5-0543-0410-b054-ed55f63600a7>
2008-07-17 03:29:46 +0000

    Entity References reach a precarious, functional balance

--- src/org/dykman/dexter/Main.java
+++ src/org/dykman/dexter/Main.java
@@ -232,8 +232,12 @@ public class Main
 			outputFile.add(f);
 		}
 
-		Writer writer = new FileWriter(f);
+		HackWriter writer = new HackWriter(new FileWriter(f));
+		Map<String,String> entities = 
+			(Map<String,String>)doc.getUserData("entity-map");
 
+		writer.setEntities(entities);
+		
 		write(doc, writer, encoding);
 		writer.close();
 	}
@@ -243,7 +247,6 @@ public class Main
 		try
 		{
 //			document.normalizeDocument();
-
 			Transformer tranformer = transformerFactory.newTransformer();
 			tranformer.setOutputProperty("indent", "no");
 			tranformer.setOutputProperty("method", "xml");
@@ -260,6 +263,136 @@ public class Main
 			throw new DexterException("error while rendering document: " 
 					+ e.getMessage(),e);
 			
+		}
+	}
+	static class HackWriter extends Writer
+	{
+		Writer inner;
+		Map<String, String> entities = null;
+		boolean started = false;
+		boolean preserveEntities = true;
+		boolean ampersandPending = false;
+		
+		HackWriter(Writer inner)
+		{
+			this.inner = inner;
+		}
+		public void setEntities(Map<String, String> entities)
+		{
+			this.entities = entities;
+		}
+		public void close()
+			throws IOException
+		{
+System.out.println("CLOSE IS CALLED");			
+			inner.close();
+		}
+		public void flush()
+			throws IOException
+		{
+			inner.flush();
+		}
+		public void writeDocType()
+			throws IOException
+		{
+			StringBuffer buffer = new StringBuffer();
+			if(entities != null && entities.size() > 0)
+			{
+				buffer.append("\n<!DOCTYPE xsl:stylesheet");
+				buffer.append(" [\n");
+				for(String key : entities.keySet())
+				{
+					buffer.append("  <!ENTITY ")
+						.append(key).append(" \"")
+						.append(entities.get(key))
+						.append("\" >\n");
+				}
+		  		buffer.append(" ]");
+				buffer.append(">\n");
+			}
+			inner.write(buffer.toString());
+		}
+		
+		public void writeWithEntities(String s)
+			throws IOException
+		{
+			if(ampersandPending)
+			{
+				int n = s.indexOf(';');
+				if(n == -1)
+				{
+					inner.write("&amp;");
+					
+				}
+				else
+				{
+					String ent = s.substring(0, n);
+					if(entities.containsKey(ent))
+					{
+						if(preserveEntities)
+						{
+							inner.write("&");
+							inner.write(ent);
+							inner.write(";");
+						}
+						else
+						{
+							inner.write(entities.get(ent));
+						}
+						s = s.substring(n+1);
+					}
+				}
+				ampersandPending = false;
+			}
+
+			int off = s.indexOf("&amp;");
+			if(off != -1)
+			{
+				if(off > 0)
+				{
+					inner.write(s.substring(0,off));
+				}
+				ampersandPending = true;
+				if(s.length() > off + 5)
+				{
+					writeWithEntities(s.substring(off+5));
+				}
+			}
+			else
+			{
+				inner.write(s);
+			}
+			
+		}
+
+		public void write(char[] ch, int off, int len)
+			throws IOException
+		{
+			String s = new String(ch,off,len);
+			
+			if(!started)
+			{
+				
+				int n = s.indexOf('>');
+				if(n == -1)
+				{
+					inner.write(s);
+				}
+				else
+				{
+					inner.write(s.substring(0,n+1));
+					started = true;
+					writeDocType();
+					if(s.length() > n+1)
+					{
+						writeWithEntities(s.substring(n, len - (n+1)));
+					}
+				}
+			}
+			else
+			{
+				writeWithEntities(s);
+			}
 		}
 	}
 }
