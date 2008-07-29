cfedda40d4ef6a2429c7814f732cb188489a5078
mdykman <mdykman@8eee1ad5-0543-0410-b054-ed55f63600a7>
2008-07-29 22:19:32 +0000

    forgot to add  galt exception into the repo

---
+++ trunk/src/org/dykman/dexter/DexterHaltException.java
@@ -0,0 +1,30 @@
+package org.dykman.dexter;
+
+
+public class DexterHaltException extends DexterException
+{
+
+	public DexterHaltException()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public DexterHaltException(String message)
+	{
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DexterHaltException(Throwable cause)
+	{
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DexterHaltException(String message, Throwable cause)
+	{
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
