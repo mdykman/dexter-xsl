2c6785f4d72be8e9a866260e7576be640555a1e0
mdykman <mdykman@8eee1ad5-0543-0410-b054-ed55f63600a7>
2009-02-16 05:56:51 +0000

    introduced 'attrs'

---
+++ trunk/src/org/dykman/dexter/dexterity/MapAttributeDescriptorS.java
@@ -0,0 +1,25 @@
+package org.dykman.dexter.dexterity;
+
+import org.dykman.dexter.descriptor.Descriptor;
+
+public class MapAttributeDescriptorS extends MapAttributeDescriptor
+{
+	public MapAttributeDescriptorS(Descriptor descriptor)
+	{
+		super(descriptor);
+	}
+	
+	@Override
+	protected String[] getTests() {
+		if(value.length() > 0 )
+		{
+			StringBuffer buffer = new StringBuffer();
+			buffer.append('[').append(value.substring(0, 1)).append(']');
+			return value.substring(1).split(buffer.toString());
+		}
+		else {
+			return  new String[0];
+		}
+	}
+	
+}
