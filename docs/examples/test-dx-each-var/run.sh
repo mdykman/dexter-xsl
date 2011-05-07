106ef9abe88416041ef0e66e8118f26419fc07d6
mdykman@gmail.com <mdykman@gmail.com@8eee1ad5-0543-0410-b054-ed55f63600a7>
2011-05-07 06:48:30 +0000

    added unit test for dx:each auto variables

--- docs/examples/test-dx-each-var/run.sh
+++ docs/examples/test-dx-each-var/run.sh
@@ -7,9 +7,9 @@ shift
 RESULT=$1
 shift
 
-DIFF=../xml-diff.sh
-#DIFF="diff -w --ignore-blank-lines"
 TMPDAT="tmp-${DATA}.res"
+#DIFF="diff -w --ignore-blank-lines"
+DIFF=../xml-diff.sh
 
 dexter.sh ${INPUT}
 dexter.sh -x${INPUT}.xsl ${DATA} > $TMPDAT
