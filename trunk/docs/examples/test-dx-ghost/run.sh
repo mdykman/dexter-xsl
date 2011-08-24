81ecc35595fd143e2a896c3bcd76070c415baea8
mdykman@gmail.com <mdykman@gmail.com@8eee1ad5-0543-0410-b054-ed55f63600a7>
2011-08-24 17:23:14 +0000

    adding new test directory

---
+++ trunk/docs/examples/test-dx-ghost/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+INPUT=$1
+shift
+DATA=$1
+shift
+RESULT=$1
+shift
+
+#DIFF="diff -w --ignore-blank-lines"
+DIFF=../xml-diff.sh
+TMPDAT="tmp-${DATA}.res"
+
+dexter.sh ${INPUT}
+dexter.sh -x${INPUT}.xsl ${DATA} > $TMPDAT
+$DIFF ${RESULT} ${TMPDAT}
+if [[ $? == "0" ]]; then
+	echo ok
+	rm $TMPDAT
+#	rm *.xsl
+	exit 0 
+else
+	echo fail
+	exit 1
+fi;
