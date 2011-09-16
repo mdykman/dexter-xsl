0e0cf3a807df2d5f3825f0f8f1ef4cd431b38066
mdykman@gmail.com <mdykman@gmail.com@8eee1ad5-0543-0410-b054-ed55f63600a7>
2011-09-16 21:20:42 +0000

    minor cosmetic update

--- trunk/src/org/dykman/dexter/TranformSource.java
+++ trunk/src/org/dykman/dexter/TranformSource.java
@@ -1,19 +1,80 @@
+/**
+ * copyright 2007, 2008 Michael Dykman
+ * user is granted unlimited use under the Artistic License
+ * http://www.opensource.org/licenses/artistic-license.php
+ */
+
 package org.dykman.dexter;
 
-import javax.xml.transform.Source;
+import java.lang.reflect.Constructor;
 
-public class TranformSource implements Source
+import org.dykman.dexter.base.PropertyResolver;
+import org.dykman.dexter.descriptor.Descriptor;
+import org.dykman.dexter.descriptor.NodeTransformDescriptor;
+import org.dykman.dexter.descriptor.TransformDescriptor;
+
+public class TransformSpecifier extends AbstractNodeSpecifier
 {
+	protected Class<TransformDescriptor> klass;
+	protected Dexter dexter;
+	protected PropertyResolver properties = null;
 
-	public String getSystemId()
+	@SuppressWarnings("unchecked")
+	public TransformSpecifier(String fqn)
+		throws Exception
 	{
-		// TODO Auto-generated method stub
-		return null;
+		this((Class<TransformDescriptor>)Class.forName(fqn));
+	}
+	public TransformSpecifier(Class<TransformDescriptor> klass)
+	{
+		this.klass = klass;
+	}
+	
+
+	@SuppressWarnings("rawtypes")
+	public Class getDescriptorClass()
+	{
+		return klass;
+	}
+	@SuppressWarnings("unchecked")
+	public void setDescriptorClass(@SuppressWarnings("rawtypes") Class klass)
+	{
+		this.klass = klass;
 	}
 
-	public void setSystemId(String systemId)
+	@SuppressWarnings("rawtypes")
+	public TransformDescriptor enclose(Descriptor descriptor)
 	{
-		// TODO Auto-generated method stub
-
+		try
+		{
+			@SuppressWarnings("unchecked")
+			Class<Object>[] parameterTypes = new Class[]
+				{ Descriptor.class };
+			@SuppressWarnings("rawtypes")
+			Constructor cc 
+				= klass
+			      .getConstructor(parameterTypes);
+			NodeTransformDescriptor td = (NodeTransformDescriptor) cc.newInstance(new Object[] {descriptor});
+			td.setPropertyResolver(properties);
+			td.setArgs(node, name, value);
+			return td;
+		}
+		catch (Exception e)
+		{
+			throw new DexterException("error instantiating descriptor",e);
+		}
 	}
+	public void setPropertyResolver(PropertyResolver properties)
+   {
+   	this.properties = properties;
+   }
+	public Dexter getDexter()
+   {
+   	return dexter;
+   }
+	public void setDexter(Dexter dexter)
+   {
+   	this.dexter = dexter;
+   }
+
 }
